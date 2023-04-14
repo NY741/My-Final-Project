7d7119a361c3604e214160fc6b75365278461332
Yusif Nuruzade <projectny@example.com>
2023-04-14 16:59:39 -0700

    Homepage created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,10 @@ const Home = () => {
         filter them using buttons with options.
       </p>
       <p>
+        News have a <mark>New</mark> sign when their release date is less than a
+        week.
+      </p>
+      <p>
         News can be <mark>sorted</mark> by:
       </p>
       <ul>
@@ -23,6 +27,22 @@ const Home = () => {
       <p>
         News can also be <mark>filtered</mark> by all the categories:
       </p>
+      <ul>
+        <li>Politics</li>
+        <li>Celebrities</li>
+        <li>Youth</li>
+        <li>Education</li>
+        <li>Business</li>
+        <li>Culture</li>
+        <li>Climate</li>
+        <li>Science</li>
+        <li>Nature</li>
+        <li>Sports</li>
+        <li>Music</li>
+        <li>Technology</li>
+        <li>Fashion</li>
+        <li>War</li>
+      </ul>
       {/* {categories.map((item) => {
         <ul>
           <li>{item}</li>
