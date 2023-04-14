541c89a08ec64005d92b45918be5b6bec7cdc69d
Yusif Nuruzade <projectny@example.com>
2023-04-14 12:41:33 -0700

    Added Date Filter

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -31,7 +31,7 @@ const News = () => {
   const addCount = 8;
 
   useEffect(() => {
-    if (!SORT_KEYS.includes(sortKey) && !('category' in query)) {
+    if (!SORT_KEYS.includes(sortKey) && !("category" in query)) {
       navigate(".");
       setSortKey();
       setSortedNews([...news]);
@@ -39,32 +39,38 @@ const News = () => {
   }, [sortKey, navigate]);
 
   function sortBy(e) {
-    generateQueryLink('sort', e)
+    generateQueryLink("sort", e);
   }
 
   function filterByCategory(e) {
-    generateQueryLink('category', e)
+    generateQueryLink("category", e);
   }
 
   function generateQueryLink(name, e) {
-    let q = []
+    let q = [];
 
     for (let key in query) {
       if (key !== name) {
-        q.push(key + '=' + query[key])
+        q.push(key + "=" + query[key]);
       }
     }
 
-    window.location.href = window.location.origin + window.location.pathname
-      + '?' + name + '=' + e.target.value + (q.length ? '&' + q.join('&') : '')
+    window.location.href =
+      window.location.origin +
+      window.location.pathname +
+      "?" +
+      name +
+      "=" +
+      e.target.value +
+      (q.length ? "&" + q.join("&") : "");
   }
 
   function showMore() {
-    setCount(count + addCount)
+    setCount(count + addCount);
   }
 
   function showAll() {
-    setCount(sortedNews.length)
+    setCount(sortedNews.length);
   }
 
   console.log(sortKey);
@@ -72,7 +78,7 @@ const News = () => {
   console.log(query);
   console.log(categories);
 
-  console.log(SORT_KEYS)
+  console.log(SORT_KEYS);
 
   // let partialNews = sortedNews.slice(0, 16);
 
@@ -83,86 +89,96 @@ const News = () => {
 
   //  }
 
+  let date = new Date();
+  date.setDate(date.getDate() - 7);
+
   return (
     <main>
       <h1>{sortKey ? `News sorted by ${sortKey}` : "News feedback"}</h1>
-     
+
       <div className="news-sort">
         <select id="news-sort-select" onChange={sortBy}>
           <option value="">Choose</option>
-          {SORT_KEYS.map(item => (
-            <option value={item} selected={sortKey === item ? 'selected' : ''}>
-              {item.charAt(0).toUpperCase()+item.slice(1)}
+          {SORT_KEYS.map((item) => (
+            <option value={item} selected={sortKey === item ? "selected" : ""}>
+              {item.charAt(0).toUpperCase() + item.slice(1)}
             </option>
           ))}
         </select>
       </div>
 
       <br></br>
-      
+
       <div className="news-filter">
         <select id="news-category-select" onChange={filterByCategory}>
           <option value="">Choose</option>
-          {categories.map(item => (
-            <option value={item} selected={query.category === item ? 'selected' : ''}>
+          {categories.map((item) => (
+            <option
+              value={item}
+              selected={query.category === item ? "selected" : ""}
+            >
               {item}
             </option>
           ))}
         </select>
       </div>
-      
+
       <br></br>
 
       <div className="card-block">
-        {sortedNews.slice(0, count).filter(v => !query.category || query.category === v.category).map((singleNews) => (
-          <div className="card">
-            <div className="card-image">
-              <img src={singleNews.img} alt={singleNews.title} />
+        {sortedNews
+          .filter((v) => !query.category || query.category === v.category)
+          .slice(0, count)
+          .map((singleNews) => (
+            <div className="card">
+              <div className="card-image">
+                <img src={singleNews.img} alt={singleNews.title} />
+              </div>
+              <div className="card-content">
+                <Link to={singleNews.id} className="card-link">
+                  <h3
+                    className={
+                      new Date(singleNews.date) >= date
+                        ? "card-title-new"
+                        : "card-title"
+                    }
+                  >
+                    {singleNews.title.length >= 45
+                      ? singleNews.title.slice(0, 45) + "..."
+                      : singleNews.title}
+                  </h3>
+                </Link>
+                <Link to={singleNews.category}>
+                  <h4 className="card-category">{singleNews.category}</h4>
+                </Link>
+                <p className="card-paragraph">
+                  {singleNews.description.length >= 120
+                    ? singleNews.description.slice(0, 120) + "..."
+                    : singleNews.description}
+                </p>
+              </div>
             </div>
-            <div className="card-content">
-              <Link to={singleNews.id} className="card-link">
-                <h3
-                  className={
-                    +singleNews.date.split("-").join("") >= 20230406
-                      ? "card-title-new"
-                      : "card-title"
-                  }
-                >
-                  {singleNews.title.length >= 45
-                    ? singleNews.title.slice(0, 45) + "..."
-                    : singleNews.title}
-                </h3>
-              </Link>
-              <Link to={singleNews.category}>
-                <h4 className="card-category">{singleNews.category}</h4>
-              </Link>
-              <p className="card-paragraph">
-                {singleNews.description.length >= 120
-                  ? singleNews.description.slice(0, 120) + "..."
-                  : singleNews.description}
-              </p>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
-      <button disabled={count >= sortedNews.length} onClick={showMore} className="btn-load">Load More</button>
-      <button disabled={count >= sortedNews.length} onClick={showAll} className="btn-load">Load All</button>
+      <div className="news-buttons">
+        <button
+          disabled={count >= sortedNews.length}
+          onClick={showMore}
+          className="btn-load"
+        >
+          Load More
+        </button>
+        <button
+          disabled={count >= sortedNews.length}
+          onClick={showAll}
+          className="btn-load"
+        >
+          Load All
+        </button>
+      </div>
     </main>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //   return (
 //     <main>
